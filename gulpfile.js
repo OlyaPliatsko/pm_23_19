10922882d90ed7dac39eb7e3654c9f91a1dac6b0
olyapl <olha.pliatsko.pm.2023@lpnu.ua>
2024-09-16 11:24:19 +0300

    Init file

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+// "use strict"
+
+// const {src, dest} = require("gulp")
+// const gulp = require("gulp")
+
+function defaultTask(cb) {
+  // place code for your default task here
+  cb();
+}
+
+exports.default = defaultTask
