b95bc673027fafafae0a7da3967544a0241cd738
olyapl <olha.pliatsko.pm.2023@lpnu.ua>
2024-10-28 22:47:35 +0200

    Initialize project files and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,21 @@
 "use strict"
 
-const { src } = require('gulp');
-src('/Users/mac/Desktop/pm_23_19.html');
-
-src('/Users/mac/Desktop/pm_23_19.html').pipe(dest('dist'));
-
-const { dest } = require('gulp');
-src('/Users/mac/Desktop/pm_23_19.html').pipe(dest('dist'));
-
-const { watch } = require('gulp');
-watch('/Users/mac/Desktop/pm_23_19/scss/*.scss', scss_task);
-
-const { series } = require('gulp');
-const build = series(html_task, scss_task, js_task);
-exports.default = build;
-
-const { parallel } = require('gulp');
+const { parallel, series } = require('gulp');
 
 const { src, dest } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
-// Таска для компіляції SCSS у CSS
 const scss_task = () => {
-return src('/Users/mac/Desktop/pm_23_19/scss/*.scss') // Вибір файлів SCSS
-.pipe(sass()) // Компіляція SCSS у CSS
-.pipe(cssnano()) // Мінімізація CSS
-.pipe(rename({suffix: '.min'})) // Додавання суфіксу .min до файлу
-.pipe(dest('dist/css')); // Збереження результату
+return src('/Users/mac/Desktop/pm_23_19/app/scss/*.scss')
+.pipe(sass())
+.pipe(cssnano())
+.pipe(rename({suffix: '.min'}))
+.pipe(dest('dist/css'));
 };
+ const html_task=()=>{
+  return src('app/*.html').pipe(dest('dist'))
+ }
 
+const build=series(html_task, scss_task)
+exports.default = build;
