366f01229e63fd8d0c6a7bedac7a2f2a1947ed43
olyapl <olha.pliatsko.pm.2023@lpnu.ua>
2024-11-04 12:30:54 +0200

    chnged html and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,64 @@
-"use strict"
+import gulp from 'gulp';
+import cssnano from 'gulp-cssnano';
+import autoprefixer from 'gulp-autoprefixer';
+import imagemin from 'gulp-imagemin';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
+import browserSync from 'browser-sync';
+import * as sass from 'sass';
+import gulpSass from 'gulp-sass';
 
-const { parallel, series } = require('gulp');
+const sassCompiler = gulpSass(sass);
 
-const { src, dest } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const cssnano = require('gulp-cssnano');
-const rename = require('gulp-rename');
-const scss_task = () => {
-return src('/Users/mac/Desktop/pm_23_19/app/scss/*.scss')
-.pipe(sass())
-.pipe(cssnano())
-.pipe(rename({suffix: '.min'}))
-.pipe(dest('dist/css'));
-};
- const html_task=()=>{
-  return src('app/*.html').pipe(dest('dist'))
- }
+gulp.task('html', function () {
+    return gulp.src('app/*.html')
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream());
+});
 
-const build=series(html_task, scss_task)
-exports.default = build;
+gulp.task('scss', function () {
+    return gulp.src('app/scss/*.scss')
+        .pipe(sassCompiler().on('error', sassCompiler.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cssnano())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('app/js/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('images', function () {
+    return gulp.src ( "app/img/*.+(jpg|jpeg|png|gif)", { encoding: false })
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            interlaced: true
+        }))
+        .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('watch', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        },
+    });
+    gulp.watch('app/*.html', gulp.series('html')).on('change', browserSync.reload);
+    gulp.watch('app/js/*.js', gulp.series('scripts'));
+    gulp.watch('app/scss/*.scss', gulp.series('scss'));
+    gulp.watch('app/img/*.+(jpg|jpeg|png|gif)', gulp.series('images'));
+});
+
+gulp.task('default', gulp.series('html', 'scss', 'scripts', 'images', 'watch'));
